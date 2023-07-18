cc2c11e784aa906c8168ee68bb54c2273b462de9
Kane Toomer <kanetoomer@yahoo.com>
2023-07-18 14:19:09 -0700

    CST 105 Topic 1 DQ 1 response

---
+++ MyFirstProject/Introduction.java
@@ -0,0 +1,12 @@
+// Kane Toomer
+// CST 105 - TOPIC 1 DQ 1
+
+public class Introduction {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Kane Toomer.");
+        System.out.println("My favorite color is purple.");
+        System.out.println("If I won the lottery, I would have a farm dedicated to fighting insecurity for Nevada Children.");
+    }
+}
